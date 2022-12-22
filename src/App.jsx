70aa1e254fb3647c3de2e8c8ef8676f62f880fb8
carlosreyes <carlosreyes8436@gmail.com>
2022-12-22 17:49:59 -0600

    add banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Header from "./components/Header"
+import MoviesBanner from "./components/Banner"
 
 function App() {
 
   return (
     <div className="App">
       <Header/>
+      <MoviesBanner />
     </div>
   )
 }
