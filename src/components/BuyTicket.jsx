43700aa3fffca78ab8bb9948062d2526800c2c47
carlosreyes <carlosreyes8436@gmail.com>
2022-12-24 13:22:36 -0600

    add asientos at BuyTicket component

--- src/components/BuyTicket.jsx
+++ src/components/BuyTicket.jsx
@@ -7,6 +7,7 @@ import {
   Text,
   VStack,
   Badge,
+  Center,
 } from "@chakra-ui/react";
 import { useMoviesContext } from "../hooks/useMoviesContext";
 import Header from "./Header";
@@ -14,18 +15,23 @@ import Header from "./Header";
 export function BuyTicket({ param }) {
   const { id } = param;
 
-  const { movies } = useMoviesContext();
-  const filteredMovies = movies.filter((movie) => movie._id == id);
+  let filas = [...Array(5)]
+  let columnas = [...Array(10)]
+
+  console.log(filas)
+
+  const { movies } = useMoviesContext()
+  const filteredMovies = movies.filter((movie) => movie._id == id)
 
   return (
     <>
       <Header />
       <Container maxW={"container.xl"} mt="15">
-        <Flex border="1px solid blue">
+        <Flex justifyContent="space-between">
             
           {filteredMovies.map((movie) => (
             <VStack className="leftChild" alignItems="flex-start" key={movie._id} gap="5">
-                <Box border="1px solid green" height="600px" w="400px">
+                <Box height="600px" w="400px">
                     <Image h="100%" src={movie.img}></Image>
                 </Box>
 
@@ -52,7 +58,33 @@ export function BuyTicket({ param }) {
             </VStack>
           ))}
 
-         <Box></Box>
+         <Box className="asientos" display="grid" placeItems="center">
+            <Box className="asientosChild">
+
+              {
+                filas.map((fila, index1) => (
+                  <Flex className={fila} key={index1} mb="20px" gap="2">
+                    {
+                      columnas.map((columna, index2) => (
+                        <Center className={columna} key={index2} w="70px" 
+                        h="70px"
+                        cursor="pointer" 
+                        border="2px solid white" 
+                        borderRadius={4}
+                        _hover={{bg: "white", color: "main"}}>
+                          <Text fontSize="2xl">{index1}, {index2}</Text>
+                        </Center>
+                        ))
+                      }
+                  </Flex>
+                ))
+              }
+            </Box>
+
+            <Button bg="white">
+              <Text fontSize="xl" fontWeight="light" color="black">Elige tu asiento</Text>
+            </Button>
+         </Box>
 
         </Flex>
       </Container>
