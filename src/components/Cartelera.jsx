5529eac6d63767d8bf4f9ae4459b17b791666986
carlosreyes <carlosreyes8436@gmail.com>
2023-01-24 23:50:01 -0600

    Format Cartelera component

--- src/components/Cartelera.jsx
+++ src/components/Cartelera.jsx
@@ -1,27 +1,32 @@
-import { Box, Container, Text, SimpleGrid, Image } from "@chakra-ui/react"
-import { Link } from "wouter"
-import { useMoviesContext } from "../hooks/useMoviesContext"
+import { Box, Container, Text, SimpleGrid, Image } from "@chakra-ui/react";
+import { Link } from "wouter";
+import { useMoviesContext } from "../hooks/useMoviesContext";
 
 export function Cartelera() {
+  const { movies } = useMoviesContext();
 
-    const { movies } = useMoviesContext()
-
-    return (
-        <Box as="section" bg="main" py={"50px"}>
-           <Text color="white" textAlign="center" fontWeight="bold" fontSize="5xl" pb={"30px"}>Cartelera</Text> 
-           <Container maxW="container.xl"> 
-                <SimpleGrid minChildWidth="250px" spacing='40px' placeItems="center">
-                    {
-                        movies.map(({img, _id}) => (
-                            <Link to={`/movie/${_id}`} key={_id}>
-                                <Box w="250px" cursor="pointer" border="2px solid white">
-                                    <Image src={img}/>
-                                </Box>
-                            </Link>
-                        ))
-                    }
-                </SimpleGrid>
-           </Container>
-        </Box>
-    )
+  return (
+    <Box as="section" bg="main" py={"50px"}>
+      <Text
+        color="white"
+        textAlign="center"
+        fontWeight="bold"
+        fontSize="5xl"
+        pb={"30px"}
+      >
+        Cartelera
+      </Text>
+      <Container maxW="container.xl">
+        <SimpleGrid minChildWidth="250px" spacing="40px" placeItems="center">
+          {movies.map(({ img, _id }) => (
+            <Link to={`/movie/${_id}`} key={_id}>
+              <Box w="250px" cursor="pointer" border="2px solid white">
+                <Image src={img} />
+              </Box>
+            </Link>
+          ))}
+        </SimpleGrid>
+      </Container>
+    </Box>
+  );
 }
